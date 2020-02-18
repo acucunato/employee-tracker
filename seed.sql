754abcfc5673f65acbcc9b669d4edb74a667b4a5
Alyssa Cucunato <acucunato18@gmail.com>
2020-02-18 17:16:17 -0500

    set up database & initial app structure

--- seed.sql
+++ seed.sql
@@ -1,4 +1,63 @@
 USE employee_trackerDB;
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Manager", 155000, 3 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4 );
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Justin", "Timberlake", 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("JC", "Chasez", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Chris", "Kirkpatrick", 3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Joey", "Fatone", 4);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Lance", "Bass", 5);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Britney", "Spears", 6);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Christina", "Aguilera", 7);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Nick", "Carter", 8);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("AJ", "McLean", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Howie", "Dorough", 4);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Kevin", "Richardson", 6);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Brian", "Littrell", 8);
--- server.js
+++ server.js
@@ -1,3 +1,85 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "yourRootPassword",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  startEmployeeManager();
+});
+
+function startEmployeeManager() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+        "exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+function viewAllEmployees() {}
+function viewAllDepartments() {}
+function viewAllRoles() {}
+function addEmployee() {}
+function addDepartment() {}
+function addRole() {}
+function updateEmployeeRole() {}
